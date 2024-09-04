3ad132b086ca9385bd80538ac5fb857aafe7fb1a
Juanes <trianajuan95@gmail.com>
2024-09-03 20:56:59 -0800

    ds

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './lab12-2/index.js', // Cambia esto a tu archivo de entrada principal
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
