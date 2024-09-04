1aa145b8afdc12117697c544a7f7f96a206efde6
Juanes <trianajuan95@gmail.com>
2024-09-03 20:59:07 -0800

    df

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './lab12-2/index.js', // Cambia esto a tu archivo de entrada principal
+  entry: './lab12-2/index.html', // Cambia esto a tu archivo de entrada principal
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
